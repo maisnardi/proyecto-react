b5e4d008363f80c805ab3943b19137b650d92c70
Matias Isnardi <maisnardi@hotmail.com>
2022-11-15 20:26:05 -0300

    se agregan estilos

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -9,18 +9,50 @@ const CartProvider = (props) => {
     //funciones del CartContext
     //funcion que agregar productos al carrito.
     const addToCart = (item, cantidad)=>{
-        
-        setCart([...cart,{...item,cantidad}])
-
+      if (isInCart(item.id)){
+        sumarUnidades(item, cantidad);
+      }
+      else{
+        setCart([...cart,{...item,cantidad}]);
+      }  
     }
 
     //funcion que verifica si el elemto se encuentra ya agregado al carrito
-    const isInCart=(item)=>{
-        return cart.some((prod)=>prod.id===item.id)
+    const isInCart=(id)=>{
+        return cart.some((prod)=>prod.id===id)
     }
     console.log(cart)
+    
+    //funcion que suma al carrito solo las unidades cuando ya existe el item en el cart
+    const sumarUnidades=(item, cantidad)=>{
+      const cartActualizado=cart.map((prodDelCarrito)=>{
+        if(prodDelCarrito.id===item.id)
+        {
+          const productoModificado = {
+            ...prodDelCarrito, cantidad:cantidad,
+          }
+          return productoModificado;
+        }
+        else{
+          return prodDelCarrito;
+        }
+      })
+      setCart(cartActualizado);
+    };
+    
+
+    //funcion para vaciar el carrito
+    const deleteAll = ()=>{
+      setCart([]);
+    }
+    //funcion para eliminar un producto
+
+    //funcion para sumar el total $ del carrito
+
+    //funcion para sumar el total de unidades del carrito (cartWidget)
+    
   return (
-    <CartContext.Provider value={{cart, addToCart}}>
+    <CartContext.Provider value={{cart, addToCart,  deleteAll}}>
         {props.children}
     </CartContext.Provider>
   )
